56ceecd610185daf70605ad652d6a043ef7fc451
Olivier Gintrand <olivier.gintrand@forterro.com>
2026-07-31 15:07:26 +0200

    chore(release): 3.12.0-forterro.12 (task board inline editors + Basic|Live|Board mode)
    Signed-off-by: Olivier Gintrand <olivier.gintrand@forterro.com>

--- backend/handlers/config.go
+++ backend/handlers/config.go
@@ -97,7 +97,7 @@ func (h *ConfigHandler) HandleConfig(w http.ResponseWriter, r *http.Request) {
 		"liveCollab":   h.liveCollab,
 		"require2FA":   h.require2FA,
 		"userProvider": h.userProvider,
-		"version":      "3.12.0-forterro.11",
+		"version":      "3.12.0-forterro.12",
 		"commit":       Commit,
 		"buildTime":    BuildTime,
 	}
