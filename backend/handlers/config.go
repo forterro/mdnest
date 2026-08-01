cce595a6f6ee4d0d392e7a5d9bc8f10f798adf55
Olivier Gintrand <olivier.gintrand@forterro.com>
2026-08-01 19:37:55 +0200

    chore(release): 3.12.0-forterro.25
    Deleting a Git Workspaces project now also removes the namespace from storage, so it leaves the namespace pickers.
    
    Signed-off-by: Olivier Gintrand <olivier.gintrand@forterro.com>

--- backend/handlers/config.go
+++ backend/handlers/config.go
@@ -106,7 +106,7 @@ func (h *ConfigHandler) HandleConfig(w http.ResponseWriter, r *http.Request) {
 		"liveCollab":   h.liveCollab,
 		"require2FA":   h.require2FA,
 		"userProvider": h.userProvider,
-		"version":      "3.12.0-forterro.24",
+		"version":      "3.12.0-forterro.25",
 		"commit":       Commit,
 		"buildTime":    BuildTime,
 	}
