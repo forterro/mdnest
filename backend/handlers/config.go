932f477e17e972bd117954a5e81a632e7e3b2ccc
Olivier Gintrand <olivier.gintrand@forterro.com>
2026-08-05 09:04:51 +0200

    chore(release): 4.1.2-forterro.11
    Signed-off-by: Olivier Gintrand <olivier.gintrand@forterro.com>

--- backend/handlers/config.go
+++ backend/handlers/config.go
@@ -114,7 +114,7 @@ func (h *ConfigHandler) HandleConfig(w http.ResponseWriter, r *http.Request) {
 		"liveCollab":   h.liveCollab,
 		"require2FA":   h.require2FA,
 		"userProvider": h.userProvider,
-		"version":      "4.1.2-forterro.10",
+		"version":      "4.1.2-forterro.11",
 		"commit":       Commit,
 		"buildTime":    BuildTime,
 	}
