c18d62f30d322c134f5089bb23191eaccffff1bb
Olivier Gintrand <olivier.gintrand@forterro.com>
2026-08-07 22:01:54 +0200

    chore(release): 4.1.2-forterro.27
    SSO sessions now use a 12h token TTL so IdP group-membership changes
    (access-group authz) take effect within 12h instead of up to a year.
    
    Signed-off-by: Olivier Gintrand <olivier.gintrand@forterro.com>

--- backend/handlers/config.go
+++ backend/handlers/config.go
@@ -124,7 +124,7 @@ func (h *ConfigHandler) HandleConfig(w http.ResponseWriter, r *http.Request) {
 		"liveCollab":   h.liveCollab,
 		"require2FA":   h.require2FA,
 		"userProvider": h.userProvider,
-		"version":      "4.1.2-forterro.26",
+		"version":      "4.1.2-forterro.27",
 		"commit":       Commit,
 		"buildTime":    BuildTime,
 	}
