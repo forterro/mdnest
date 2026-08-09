1d621cdf6529ed6777def4763c869624f84ac59c
Olivier Gintrand <olivier.gintrand@forterro.com>
2026-08-09 14:57:47 +0200

    chore(release): 4.1.2-forterro.31
    Task board: task titles containing a comma no longer break relation lists
    (values are quoted on write and the parser honors quoted segments).
    
    Signed-off-by: Olivier Gintrand <olivier.gintrand@forterro.com>

--- backend/handlers/config.go
+++ backend/handlers/config.go
@@ -124,7 +124,7 @@ func (h *ConfigHandler) HandleConfig(w http.ResponseWriter, r *http.Request) {
 		"liveCollab":   h.liveCollab,
 		"require2FA":   h.require2FA,
 		"userProvider": h.userProvider,
-		"version":      "4.1.2-forterro.30",
+		"version":      "4.1.2-forterro.31",
 		"commit":       Commit,
 		"buildTime":    BuildTime,
 	}
